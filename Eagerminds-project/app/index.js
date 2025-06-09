e5ad478aaba8cadccb2c5baedea8749944edce92
Tarun Gagiya <155285876+Tarungagiya@users.noreply.github.com>
2025-06-09 16:47:24 +0530

    Second commit

--- Eagerminds-project/app/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('Hello it is Eagerminds project');
-});
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
